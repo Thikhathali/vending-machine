82c7dd378d5521d5a7c1a976d776af4c95531f3a
Thikhathali <thikhamataboge@gmail.com>
2023-05-16 20:29:29 +0200

    Added main file

---
+++ main.py
@@ -0,0 +1,22 @@
+#! python3
+
+# main.py 
+# A module where instances are defined
+
+import wallet, vendingMachine
+
+wallet_bal = wallet.Wallet()
+v_machine = vendingMachine.VendingMachine()
+
+print(f'current Bal: R{wallet_bal.get_balance()}\n')
+v_machine.display_products()
+
+select_item = int(input('Pleases select item ->\n'))
+print(f'Is available? {v_machine.check_availability(select_item)}')
+print(f'Can afford? {v_machine.check_funds(select_item)}')
+v_machine.dispense_product(select_item)
+
+# v_machine.display_products()
+
+
+
---
+++ vendingMachine.py
@@ -0,0 +1,47 @@
+#! python3
+
+# vendingMachine.py
+# A module where transactions occur
+
+import products, wallet
+
+class VendingMachine:
+    
+    lstStockItems = [
+        products.Product("Coke", 15.0, 5),
+        products.Product("Chips", 8.0, 3),
+        products.Product("Choco", 12.0, 5),
+        products.Product("Lays", 8.0, 0),
+        products.Product("Candy", 11.0, 5),
+    ]
+    productAvailable = True
+    afford = True
+    wallet_bal = wallet.Wallet()
+
+    def display_products(self) -> None:
+        for i in range(len(self.lstStockItems)):
+            print(f'<-- it {i + 1} em -->')
+            print(self.lstStockItems[i])
+
+    def check_availability(self, choice) -> bool:
+        self.productAvailable = False
+        if self.lstStockItems[choice - 1].get_quantity() > 0:
+            self.productAvailable = True
+        return self.productAvailable
+    
+    def check_funds(self, choice) -> bool:
+        self.afford = False
+        if self.lstStockItems[choice - 1].get_price() < self.wallet_bal.get_balance():
+            self.afford = True
+        return self.afford
+    
+    def dispense_product(self, choice) -> None:
+        if not self.productAvailable:
+            print('Item not avaialbe ..')
+        if not self.afford:
+            print('Insufficient funds!')         
+        elif self.productAvailable == True and self.afford == True:
+            new_quantity = self.lstStockItems[choice - 1].get_quantity() - 1
+            self.lstStockItems[choice - 1].set_quantity(new_quantity)
+            print(f'{self.lstStockItems[choice - 1].get_name()} dispensing ...')
+            print(f'new quantity: {self.lstStockItems[choice - 1].get_quantity()}')
